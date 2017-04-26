5a8f8c6762028548cd81ac5fac2c86857ca189cf
lycte <396136337@qq.com>
2017-04-26 20:42:06 +0800

    更改数据

--- node+express+mongdb/myapp/db.js
+++ node+express+mongdb/myapp/db.js
@@ -1,36 +1,75 @@
+/*!!!!!!!!增加数据！！！！！！！！！！！
+ //引入mongodb模块，获得客户端对象
+ var MongoClient = require('mongodb').MongoClient
+ //连接字符串
+ var DB_CONN_STR = 'mongodb://localhost:27017/gomall'
+
+ //定义函数表达式，用于操作数据库并返回结果
+
+ var insertData = function (db,callback) {
+ //获得制定的集合
+ var collection  = db.collection('users');
+ //插入数据
+ var data = [{_id:70,"name":'rose',"age":21},{_id:80,"name":'mark',"age":22}];
+ collection.insert(data,function (err,result) {
+ //如果存在错误
+ if(err){
+ console.log('Error:'+err);
+ return;
+ }
+ //调用传入的回调方法，将操作结果返回
+ callback(result);
+ })
+ }
+
+ //使用客户端连接数据，并指定完成时的回调方法
+
+ MongoClient.connect(DB_CONN_STR,function (err,db) {
+ console.log("连接成功！");
+ //console.log(err);
+ //执行插入数据操作，调用自定义方法
+ insertData(db,function (result) {
+ //显示结果
+ console.log(result);
+ //关闭数据库
+ db.close();
+ })
+ })
+* */
+
 //引入mongodb模块，获得客户端对象
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
 //连接字符串
-var DB_CONN_STR = 'mongodb://localhost:27017/gomall'
+var DB_CONN_STR = 'mongodb://localhost:27017/gomall';
 
 //定义函数表达式，用于操作数据库并返回结果
-
-var insertData = function (db,callback) {
-    //获得制定的集合
-    var collection  = db.collection('users');
-    //插入数据
-    var data = [{_id:70,"name":'rose',"age":21},{_id:80,"name":'mark',"age":22}];
-    collection.insert(data,function (err,result) {
+var updateData = function(db, callback) {
+    //获得指定的集合
+    var collection = db.collection('users');
+    //要修改数据的条件，>=10岁的用户
+    var  where={age:{"$gte":10}};
+    //要修改的结果
+    var set={$set:{age:9}};
+    collection.updateMany(where,set, function(err, result) {
         //如果存在错误
-        if(err){
-            console.log('Error:'+err);
+        if(err)
+        {
+            console.log('Error:'+ err);
             return;
         }
         //调用传入的回调方法，将操作结果返回
         callback(result);
-    })
+    });
 }
 
 //使用客户端连接数据，并指定完成时的回调方法
-
-MongoClient.connect(DB_CONN_STR,function (err,db) {
-   console.log("连接成功！");
-   //console.log(err);
-   //执行插入数据操作，调用自定义方法
-    insertData(db,function (result) {
+MongoClient.connect(DB_CONN_STR, function(err, db) {
+    console.log("连接成功！");
+    //执行插入数据操作，调用自定义方法
+    updateData(db, function(result) {
         //显示结果
         console.log(result);
         //关闭数据库
         db.close();
-    })
-})
+    });
+});
